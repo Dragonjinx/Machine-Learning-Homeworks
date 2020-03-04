12ef23fa0be81a187540f80568d80deb877dabbd
Vlada <vlada.v.petrusenko@gmail.com>
2020-03-04 21:36:46 +0100

    final
    :D

--- Homework2/scatter_data.py
+++ Homework2/scatter_data.py
@@ -19,15 +19,11 @@ y = [None] * 100
 for i in range(100):
     c = [(b[0], abs(b[0] - x[i])),(b[1], abs(b[1] - x[i])),(b[2], abs(b[2] - x[i])),(b[3], abs(b[3] - x[i])),(b[4], abs(b[4] - x[i]))]
     c.sort(key=takeSecond)
-    #d = c[:k]
     average = 0
     for j in range(k):
-        print(takeFirst(c[j]), dict[takeFirst(c[j])])
         average += dict[takeFirst(c[j])]
     average /= k
     y[i] = average
-
-#x,y = 4*(np.random.rand(2,100))
 
 plt.plot(x,y, 'o')
 plt.plot(b,a, 'o')
